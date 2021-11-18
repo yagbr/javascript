d902d17a0910d614226975066dc08d58f8ec33e6
DESKTOP-UHB1KG7\Lenovo <yag@gmail.com>
2021-11-18 19:29:32 +0530

    first commit

---
+++ data_type.js
@@ -0,0 +1,15 @@
+// var myName ="yagnik";
+// console.log (myName);
+
+// var myNumber =26;
+// console.log(myNumber);
+
+var imName =true;
+//  typeof operators
+// console.log(typeof(myName));
+// console.log(typeof(myNumber));
+// console.log(typeof(imName));
+
+//type pradical
+// 10 + "20"
+console.log(10+"20");
